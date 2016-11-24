dbca9c5191bade92b17e5feca7bcc0638041d80e
Valerie <valdeanda@ciencias.unam.mx>
2016-11-24 13:50:17 +0100

    Update get_sps_uniprot.sh

--- get_sps_uniprot.sh
+++ get_sps_uniprot.sh
@@ -4,7 +4,7 @@ while read line
 do
 uniprot="$line"
 #echo "reading :$line"
-grep $line --after-context=1  /home/valerie/familias_reconstruccion/uniprot_trembl.fasta  >> $filename.names.fa
+grep $line --after-context=1  /path/to/uniprot_trembl.fasta  >> $filename.names.fa
 done <"$filename"
 
 
