540cbf02d2c2249edc2d84704ee9405759025970
Valerie <valdeanda@ciencias.unam.mx>
2017-03-13 22:04:38 +0100

    Update get_sps_uniprot.sh

--- get_sps_uniprot.sh
+++ get_sps_uniprot.sh
@@ -7,5 +7,9 @@ uniprot="$line"
 grep $line --after-context=1  /path/to/uniprot_trembl.fasta  >> $filename.names.fa
 done <"$filename"
 
+#http://www.ebi.ac.uk/uniprot/database/download.html
+
+wget "ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/uniprot_trembl.fasta.gz"
+
 
 
